2ecfea6081139157f798865b2c30207c45227376
Raj Shah <raj.shah@comparethemarket.com>
2024-06-12 23:18:57 +0100

    Euro24Draw with config driven loaders classes. Designed to randomly assign teams

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
-import random
-import pprint
+from draws import random_assigned_draw, raj_cheats
+from writer import write_results_to_yml
+from animations import animate_drums, banner
+import time
 
-names = ["Raj", "Jay", "Jash", "Mausmi", "Akash", "Jinali", "Dipal", "Ron", "Nakul", "Disha", "Viral", "Neeti", "Khush", "Dhaval", "Mudra", "Juhi"]
-seed_high_a = ["France", "Argentina", "England", "Mexico", "Netherlands", "Denmark", "United States", "Wales"]
-seed_high_b = ["Brazil", "Belgium", "Spain", "Portugal", "Germany", "Uruguay", "Switzerland", "Croatia"]
-seed_low_a = ["Senegal", "Iran", "Poland", "Tunisia", "Australia", "Ecuador", "Saudi Arabia", "Qatar"]
-seed_low_b = ["Japan", "Morocco", "Serbia", "South Korea", "Costa Rica", "Cameroon", "Canada", "Ghana"]
+if __name__ == '__main__':
+    results = random_assigned_draw()
 
-sweepstakes = {}
+    write_results_to_yml(results)
 
-for i in range(1):
-    seed_high = seed_high_a + seed_high_b
-
-    random.shuffle(names)
-    random.shuffle(seed_high)
-    random.shuffle(seed_low_a)
-    random.shuffle(seed_low_b)
-
-    a = 0
-    b = 0
-
-    for index, (name, team_high) in enumerate(zip(names, seed_high)):
-        sweepstakes[name] = team_high
-
-    for name, team_high in sweepstakes.items():
-        if team_high in seed_high_a:
-            team_low = seed_low_b[b]
-            sweepstakes[name] = (team_high, team_low)
-            b += 1
-        else:
-            team_low = seed_low_a[a]
-            sweepstakes[name] = (team_high, team_low)
-            a += 1
-
-    pprint.pprint(sweepstakes)
-    sweepstakes = {}
+    for player, teams in results.items():
+        animate_drums()
+        banner(player, teams)
+        time.sleep(3)
---
+++ writer.py
@@ -0,0 +1,24 @@
+import yaml
+
+
+def write_results_to_yml(results) -> None:
+    with open("results.yml", "w") as file:
+        yaml.dump(results, file)
+
+
+def raj_cheats(final_draw) -> None:
+    pass
+
+    """
+    HMMMPPPHHH!
+
+    I see how it is!
+    You don't trust me do you!?
+
+    Well I could have easily cheated...
+    AND I DIDN'T!
+    """
+
+    """
+    ... Or did?
+    """
